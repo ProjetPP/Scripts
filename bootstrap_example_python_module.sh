32981418c0430cd3f59ed38079b67ab25413d8b5
Valentin Lorentz <progval@progval.net>
2014-10-19 16:49:52 +0200

    Add comments to the output.

--- bootstrap_example_python_module.sh
+++ bootstrap_example_python_module.sh
@@ -33,9 +33,9 @@ echo "{
 
 
 echo "Bootstrapped! Now you just have to run in two different terminals:"
-echo "  PPP_CORE_CONFIG=/tmp/ppp_core_config.json gunicorn ppp_core:app -b 127.0.0.1:8000"
-echo "  gunicorn example_ppp_module:app -b 127.0.0.1:8001"
+echo "  PPP_CORE_CONFIG=/tmp/ppp_core_config.json gunicorn ppp_core:app -b 127.0.0.1:8000 # Start the router and make it listen on port 8000"
+echo "  gunicorn example_ppp_module:app -b 127.0.0.1:8001 # Start the module and make it listen on port 8001 (the config tells the router which port it listens on)"
 echo ""
 echo "Then, you can test it from a Python shell:"
-echo ">>> import requests"
-echo ">>> requests.post('http://127.0.0.1:8000', data='{\"language\": \"en\", \"tree\": {\"type\": \"triple\", \"subject\": {\"type\": \"resource\", \"value\": \"you\"}, \"object\": {\"type\": \"missing\"}, \"predicate\": {\"type\": \"resource\", \"value\": \"be\"}}}').json()"
+echo ">>> import requests # Import requests, a nice HTTP library"
+echo ">>> requests.post('http://127.0.0.1:8000', data='{\"language\": \"en\", \"tree\": {\"type\": \"triple\", \"subject\": {\"type\": \"resource\", \"value\": \"you\"}, \"object\": {\"type\": \"missing\"}, \"predicate\": {\"type\": \"resource\", \"value\": \"be\"}}}').json() # Make a request to the router"
