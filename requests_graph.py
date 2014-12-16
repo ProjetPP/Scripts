edbeb9fead575730db478923d0b3c0c36b22d9c5
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2014-12-16 23:00:48 +0100

    Allow to play on the granulometry (from 1 min to 1 hour).

--- requests_graph.py
+++ requests_graph.py
@@ -9,7 +9,8 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 
-NB_HOURS = 2
+NB_HOURS = 24
+GRANULOMETRY = 15 # must be a divisor of 60
 
 if len(sys.argv) != 2:
     print('Syntax: %s file.png' % sys.argv[0])
@@ -27,16 +28,16 @@ data = [now - x for x in data]
 max_ = datetime.timedelta(hours=NB_HOURS)
 
 # Shrink and convert to minutes
-data = [x.seconds//60 for x in data if x <= max_]
+data = [x.seconds//(60*GRANULOMETRY) for x in data if x <= max_]
 
 # Compute the height of the bars
-requests_per_minute = array.array('I', (0 for x in range(0, 60*NB_HOURS)))
+requests_per_minute = array.array('I', (0 for x in range(0, 60*NB_HOURS//GRANULOMETRY)))
 for x in data:
-    requests_per_minute[(60*NB_HOURS) - x - 1] += 1
+    requests_per_minute[(60*NB_HOURS//GRANULOMETRY) - x - 1] += 1
 
 
 # Final plot
-x = range(0, 60*NB_HOURS)
+x = range(0, 60*NB_HOURS//GRANULOMETRY)
 plt.plot(x, requests_per_minute, label=None)
 plt.title("Requests per minute")
 plt.xlabel("Time (minutes)")
