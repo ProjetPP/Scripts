a2c063d5ec573e71f02d2f1c0cc3219d0642e002
Valentin Lorentz <progval@progval.net>
2014-12-23 11:25:51 +0100

    Human-reable x-axis (first item of GH-2).

--- requests_graph.py
+++ requests_graph.py
@@ -8,13 +8,17 @@ import requests
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
-NB_HOURS = 23
-GRANULOMETRY = 10 # must be a divisor of 60
+NB_HOURS = 48
+GRANULOMETRY = 60 # must be a divisor of 60
 
 if len(sys.argv) != 2:
     print('Syntax: %s file.png' % sys.argv[0])
     exit(1)
+
+# Initialize matplotlib
+fig, ax = plt.subplots()
 
 # Get data
 data = requests.get('http://logger.frontend.askplatyp.us/', params={'limit': 10000}).json()
@@ -23,24 +27,33 @@ data = requests.get('http://logger.frontend.askplatyp.us/', params={'limit': 100
 data = [datetime.datetime(*time.strptime(x[1].split('.')[0], "%Y-%m-%d %H:%M:%S")[:6]) for x in data]
 
 # Compute the difference
-now = datetime.datetime.now()
-data = [now - x for x in data]
-max_ = datetime.timedelta(hours=NB_HOURS)
+datemax = datetime.datetime.now()
+data = [datemax - x for x in data]
+delta = datetime.timedelta(hours=NB_HOURS)
+datemin = datemax - delta
+ax.set_xlim(datemin, datemax)
 
 # Shrink and convert to minutes
-data = [x.seconds//(60*GRANULOMETRY) for x in data if x <= max_]
+data = [x.seconds//(60*GRANULOMETRY) for x in data if x <= delta]
 
 # Compute the height of the bars
 requests_per_minute = array.array('I', (0 for x in range(0, 60*NB_HOURS//GRANULOMETRY)))
 for x in data:
     requests_per_minute[(60*NB_HOURS//GRANULOMETRY) - x - 1] += 1
 
+# Units
+hours    = mdates.HourLocator(interval=6) # Show ticks every 6 hour
+hoursFmt = mdates.DateFormatter('%H:00')
+ax.xaxis.set_major_locator(hours)
+ax.xaxis.set_major_formatter(hoursFmt)
 
 # Final plot
-x = list(map(lambda x:x*GRANULOMETRY, range(-60*NB_HOURS//GRANULOMETRY, 0)))
-plt.plot(x, requests_per_minute, label=None)
+x = list(map(lambda x:datemin + datetime.timedelta(minutes=x*GRANULOMETRY), range(0, 60*NB_HOURS//GRANULOMETRY)))
+ax.plot(x, requests_per_minute, label=None)
+
+# Titles
 plt.title("Requests to the PPP in the last %d hours" % NB_HOURS)
-plt.xlabel("Time (minutes)")
+plt.xlabel("Time (hours)")
 plt.ylabel("Requests (per slice of %d minutes)" % GRANULOMETRY)
 #plt.legend()
 plt.savefig(sys.argv[1])
