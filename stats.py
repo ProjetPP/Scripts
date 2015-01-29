68ea38663050faff4b908a0976a9b1f156a8cfc4
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2015-01-29 18:33:06 +0100

    Fix a bug in stat.py (did not fetch all repositories).

--- stats.py
+++ stats.py
@@ -1,52 +1,58 @@
-import github
 import requests
-from requests_oauthlib import OAuth2
 import pickle
 import sys
 
-def print_stats(token):
-    ppp_repos=requests.get('https://api.github.com/orgs/ProjetPP/repos',params={'access_token':token}).json()
-    commit_counts = {}
-    total = 0
-    print("%d repositories found." % len(ppp_repos))
-    for repo in ppp_repos:
-        print(repo['name'])
-        repo_commits = requests.get(repo['commits_url'].split('{')[0],params={'access_token':token})
-        while(repo_commits.ok):
-            for commit in repo_commits.json():
-                if 'bump' not in commit['commit']['message'].lower() and 'merge' not in commit['commit']['message'].lower():
-                    total+=1
-                    try:
-                        login = commit['author']['login']
-                    except KeyError:
-                        continue
-                    except TypeError:
-                        continue
-                    try:
-                        commit_counts[login] += 1
-                    except KeyError:
-                        commit_counts[login] = 1
-            try:
-                repo_commits = requests.get(repo_commits.links['next']['url'],params={'access_token':token})
-            except KeyError:
-                break
+def printStats(token):
+    repoRequest=requests.get('https://api.github.com/orgs/ProjetPP/repos',params={'access_token':token})
+    commitCounts = {}
+    totalCommit = 0
+    totalRepo = 0
+    while True: # I have a dream that one day "do ... while" statement will exist in python
+        pppRepos = repoRequest.json()
+        totalRepo += len(pppRepos)
+        for repo in pppRepos:
+            print(repo['name'])
+            repoCommits = requests.get(repo['commits_url'].split('{')[0],params={'access_token':token})
+            while(repoCommits.ok):
+                for commit in repoCommits.json():
+                    if 'bump' not in commit['commit']['message'].lower() and 'merge' not in commit['commit']['message'].lower():
+                        totalCommit+=1
+                        try:
+                            login = commit['author']['login']
+                        except KeyError:
+                            continue
+                        except TypeError:
+                            continue
+                        try:
+                            commitCounts[login] += 1
+                        except KeyError:
+                            commitCounts[login] = 1
+                try:
+                    repoCommits = requests.get(repoCommits.links['next']['url'],params={'access_token':token})
+                except KeyError:
+                    break
+        try:
+            repoRequest = requests.get(repoRequest.links['next']['url'], params={'access_token':token})
+        except KeyError:
+            break
     print('')
-    print("Total: %d\n" % total)
-    scores = sorted(commit_counts.items(),key = lambda x: -x[1])
+    print("%d repositories found." % totalRepo)
+    print("%d commits found." % totalCommit)
+    scores = sorted(commitCounts.items(),key = lambda x: -x[1])
     for (author,nbCommits) in scores:
-        print("{0}: {1} ({2:.1f}%)".format(author.ljust(20),str(nbCommits).ljust(5),nbCommits/total*100))
+        print("{0}: {1} ({2:.1f}%)".format(author.ljust(20),str(nbCommits).ljust(5),nbCommits/totalCommit*100))
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit('Syntax: %s <token file (pickle format)>' % sys.argv[0])
     try:
-        token_file = open(sys.argv[1],'rb')
-        token = pickle.load(token_file)
-        token_file.close()
+        tokenFile = open(sys.argv[1],'rb')
+        token = pickle.load(tokenFile)
+        tokenFile.close()
     except FileNotFoundError:
         sys.exit('The file %s does not exist.' % sys.argv[1])
     except pickle.UnpicklingError:
         sys.exit('The token was not saved with pickle.')
     if not isinstance(token,str):
         sys.exit('The token is not a string.')
-    print_stats(token)
+    printStats(token)
